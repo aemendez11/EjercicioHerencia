c1a79a2d77556769c5603b8d4f2f2a2080d3a298
aemendez <aemendez@umes.edu.gt>
2024-04-29 08:46:00 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioHerencia/Catedratico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjercicioHerencia
+{
+    public class Catedratico:Padre
+    {
+        public string Titulo { get; set; }
+        public int Colegiado { get; set; }
+        public Catedratico()
+        {
+            Titulo= string.Empty;
+        }
+    }
+}
